82de0c7b6046864289b9d949b013f5e06b27346b
Sjasvin93 <sjasvin9493@gmail.com>
2025-04-27 08:57:40 +0530

    login and signup page ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+import Login from "./pages/Auth/Login"
+import SignUp from "./pages/Auth/SignUp"
+import Expenses from "./pages/Dashboard/Expenses"
+import Home from "./pages/Dashboard/Home"
+import Income from "./pages/Dashboard/Income"
+
 function App() {
 
   return (
     <div>
-      Hello
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expenses" exact element={<Expenses />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
 
-export default App
+export default App;
+
+const Root = () => {
+  const isAuthenticated = localStorage.getItem("token");
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+}
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,8 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleEmailInput = (email) => {
+    setError(" ");
+    setEmail(email);
+  }
+
+  const handlePasswordInput = (password) => {
+    setError("");
+    setPassword(password);
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if(!password){
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    //Login API Call
+  }
+
   return (
-    <div>login</div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'> Welcome Back</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          Please enter your details to log in
+        </p>
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={(e) => handleEmailInput(e.target.value)}
+            label="Email address"
+            placeholder="Enter your email address"
+            type="text"
+          />
+          <Input
+            value={password}
+            onChange={(e) => handlePasswordInput(e.target.value)}
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+          />
+
+          {
+            error &&
+            <p className='text-red-500 text-xs pb-2.5'>
+              {error}
+            </p>
+          }
+
+          <button type="submit" className='btn-primary'>
+            LOGIN
+          </button>
+
+          <p className='text-[13px] text-slate-800 mt-3'>
+            Don't have an account?{" "}
+            <Link className='font-medium text-primary underline' to="/signup">
+              Signup
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
   )
 }
 
